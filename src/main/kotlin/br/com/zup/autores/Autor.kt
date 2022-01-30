505363475540c487b84e8c73eda5915a6af625d8
rodrigodeoliveirazup <oliveirarodrigo19.g@gmail.com>
2022-01-30 12:22:01 -0300

    Conversao de obj de entrada para obj de dominio

---
+++ src/main/kotlin/br/com/zup/autores/Autor.kt
@@ -0,0 +1,9 @@
+package br.com.zup.autores
+
+class Autor(
+    val nome: String,
+    val email: String,
+    val descricao: String
+) {
+
+}
--- src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -3,12 +3,16 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class CadastraAutorController {
 
     @Post
-    fun cadastra(@Body request: NovoAutorRequest){
-        println(request)
+    fun cadastra(@Body @Valid request: NovoAutorRequest){
+        val autor = request.paraAutor()
+        println(autor.nome)
     }
 }
